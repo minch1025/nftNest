8ac9ab4d85e33d2faf454b1519bd41296cc90fab
권민초 <gwonmincho@gwonminchoui-Macmini.local>
2022-04-05 18:58:59 +0900

    20220405-2

--- src/nft/nft.controller.ts
+++ src/nft/nft.controller.ts
@@ -1,14 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { NftService } from './nft.service';
+import { User } from './entities/nft.entity'
 
 @Controller('nft')
 export class NftController {
-    constructor(private readonly nftService: NftService  )  {}
-
+    //constructor 초깃값 
+    constructor(private readonly nftService: NftService  )  {
+      // this.nftService = nftService 
+    }
+    //req,res 가 없어어--
     @Get()
     getMinch(): string  {
+      //객체를 내부에서 만들어서 객체에서다른 객체에 의존하면 결합성이 강해져서 단일 책임 분리가 어긋나게되는것 최대한 this. 활용해서 나눠줌
       return this.nftService.getminch();
     }
+
+    @Get('/All')
+    getParams(): User[] {
+      return this.nftService.getAll();
+    }
+
+    @Post('/All')
+    createName(@Body('id') id:number,@Body('name') name:string, @Body('number') age: number , @Body('address') address: string):string {
+        return this.nftService.createName();
+    }
+
+    @Get('/:id')
+    getUserNumber(@Param('id') id:string ) {
+      return `This will return one movie with the id: ${id}`
+    }
+
+    // @Post('/create_minch')
+    // postMinch(@Body('id') id:number, @Body('name') name:string):  NFT[]  {
+    //   return this.nftService.postMinch(id , name);
+    // }
     }
 
 
--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException  } from '@nestjs/common';
+import { User } from './entities/nft.entity';
+
 
 @Injectable()
 export class NftService {
-    getminch(): string  {
-        return 'Hello mincho';
-    }
+  private users:User[] = [];
+  getminch(): string  {
+    return 'Hello mincho';
+  }
+
+  getAll(): User[] {
+    return this.users;
+  }
+  createName(): string {
+    return 
+  }
+// postMinch(id: number, name: string): NFT[] {
+//   return nfts.concat({ id, name });
+// }
+
 }
